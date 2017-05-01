af518f303dcd3afd0c4146b350c20bdb3b923538
luckysher <luckyshera95@gmail.com>
2017-05-01 09:58:03 +0530

    added fun for generating password token

--- utils.py
+++ utils.py
@@ -3,7 +3,10 @@
 ##.........................................##
 
 import logging
-APP_NAME = 'Falcony'
+import base64
+import hashlib
+from settings import APP_NAME
+
 
 # function for getting logger
 def getLogger():
@@ -11,3 +14,16 @@ def getLogger():
     logger = logging.getLogger(APP_NAME)
     return logger
 
+
+# function for getting token from the string
+def strToToken(password):
+    utfEncStr = password.encode('utf-8')
+    token = None
+    try:
+        sha_1 = hashlib.sha1()
+        sha_1.update(utfEncStr)
+        token = base64.b64encode(sha_1.hexdigest())
+    except Exception as e:
+        print("Got exception while generating token: %s" % e)
+
+    return token

10c1bfdc090c3d2b715f8ebf0689844ac86538c6
luckysher <luckyshera95@gmail.com>
2017-05-04 16:48:14 +0530

    updated json translator middleware

--- middleware/jsonTranslator.py
+++ middleware/jsonTranslator.py
@@ -16,14 +16,20 @@ class JSONTranslator(object):
         #  check if any data is provided
         if req.content_length is None:
             return
-
-        if req.content_type == 'application/json':
+        if req.content_type == 'application/json' or req.content_type == 'text/plain':
             try:
                 raw_json = req.stream.read()
             except Exception as e:
                 raise falcon('bad request', e)
 
             try:
-                req.context['data'] = json.loads(raw_json)
+                req.context['data'] = raw_json
             except ValueError:
                 raise InvalidError('json decoding error or malformed json ')
+
+
+    def process_response(self, req, resp, resource):
+        if 'data' not in req.context:
+            return
+
+

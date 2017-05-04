8d182f254bd025a79a02a24874cf78b16b36531b
luckysher <luckyshera95@gmail.com>
2017-05-04 16:55:17 +0530

    added function for checking user authentication

--- dbUtils.py
+++ dbUtils.py
@@ -5,3 +5,17 @@ from sqlalchemy.orm import sessionmaker
 import traceback
 from utils import strToToken
 from settings import db_url
+
+
+def isUserAuthenticated(session, username, password):
+    token = None
+    try:
+        user = session.query(User).filter(User.username==username).first()
+        password = strToToken(password)
+        if password == user.password:
+            token = user.password
+            return token
+    except Exception as e:
+        print('Got exception while getting user..')
+        print(e)
+    return token
